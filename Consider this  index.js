a643c0944567c5039b26c55624d72d87d9515ac2
wowankit378 <133545485+wowankit378@users.noreply.github.com>
2024-04-05 11:50:42 -0400

    Create Consider this  index.js

---
+++ Consider this  index.js
@@ -0,0 +1,32 @@
+
+var express = require("express");
+// var router = express.Router();
+// let indexController = require('../controllers/index');
+// let mongoose = require("mongoose");
+// let Tournament = require("../models/tournament");
+
+// /* GET home page. */
+// router.get("/", function (req, res, next) {
+//   res.render("index", { title: "Landing" });
+// });
+
+// /* GET Home page. */
+// router.get("/home", function (req, res, next) {
+//   res.render("index", { title: "Landing" });
+// });
+
+// //router to get login display
+// router.get("/login", indexController.displayLoginPage);
+// //router to get login process
+// router.post("/login", indexController.processLoginPage);
+
+// //router for register display
+// router.get("/register", indexController.displayRegisterPage);
+
+// //router for register process
+// router.post("/register", indexController.processRegisterPage);
+
+// //logout
+// router.get("/logout", indexController.performLogout);
+
+// module.exports = router;
