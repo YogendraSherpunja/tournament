2da9d9e3fe0e6837c901d52a55aecc440d0a6f65
Yogendra Magar <yogendramagar15@gmail.com>
2024-04-10 19:14:22 -0400

    Change API url

--- client/src/constants/api.config.js
+++ client/src/constants/api.config.js
@@ -1,5 +1,6 @@
 const API_CONFIG = {
-  API_URL: "http://localhost:8000",
+  // API_URL: "http://localhost:8000",
+  API_URL: "https://tournament-b1ly.onrender.com",
 };
 
 export default API_CONFIG;
