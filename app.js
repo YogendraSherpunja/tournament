bf7ebaf39d28b58822c7b3bdee2cd201ad82f612
Yogendra Magar <yogendramagar15@gmail.com>
2024-04-10 18:42:29 -0400

    Testing

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 8000;
 
 // const __filename = fileURLToPath(import.meta.url);
 
-const __dirname = path.dirname(__filename);
+// const __dirname = path.dirname(__filename);
 
 // Middleware
 
